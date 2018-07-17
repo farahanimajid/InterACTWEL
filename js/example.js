38ddb32beb56d2cb0f62d528012da4d7938c4133
farahanimajid <33735512+farahanimajid@users.noreply.github.com>
2018-07-17 10:43:59 -0700

    Add files via upload

---
+++ js/example.js
@@ -0,0 +1,71 @@
+var tasks = [
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 0"),"endDate":new Date("Sun Nov 25 00:00:00 EST 1"),"taskName":"Fwr_1","status":"Fwr"},
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 1"),"endDate":new Date("Sun Nov 25 00:00:00 EST 2"),"taskName":"Fwr_1","status":"Fwr"},
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 2"),"endDate":new Date("Sun Nov 25 00:00:00 EST 3"),"taskName":"Fwr_1","status":"Fwr"},
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 3"),"endDate":new Date("Sun Nov 25 00:00:00 EST 4"),"taskName":"Fwr_1","status":"Fwr"},
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 4"),"endDate":new Date("Sun Nov 15 00:00:00 EST 5"),"taskName":"Fwr_1","status":"Fwr"},
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 5"),"endDate":new Date("Sun Nov 25 00:00:00 EST 6"),"taskName":"Fwr_1","status":"Fwr"},
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 6"),"endDate":new Date("Sun Nov 25 00:00:00 EST 7"),"taskName":"Fwr_1","status":"Fwr"},
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 7"),"endDate":new Date("Sun Nov 25 00:00:00 EST 8"),"taskName":"Fwr_1","status":"Fwr"},
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 8"),"endDate":new Date("Sun Nov 25 00:00:00 EST 9"),"taskName":"Fwr_1","status":"Fwr"},
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 9"),"endDate":new Date("Sun Nov 15 00:00:00 EST 10"),"taskName":"Fwr_1","status":"Fwr"},
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 10"),"endDate":new Date("Sun Nov 25 00:00:00 EST 11"),"taskName":"Fwr_1","status":"Fwr"},
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 11"),"endDate":new Date("Sun Nov 25 00:00:00 EST 12"),"taskName":"Fwr_1","status":"Fwr"},
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 12"),"endDate":new Date("Sun Nov 25 00:00:00 EST 13"),"taskName":"Fwr_1","status":"Fwr"},
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 13"),"endDate":new Date("Sun Nov 25 00:00:00 EST 14"),"taskName":"Fwr_1","status":"Fwr"},
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 14"),"endDate":new Date("Sun Nov 25 00:00:00 EST 15"),"taskName":"Fwr_1","status":"Fwr"},
+
+
+
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 0"),"endDate":new Date("Sun Nov 15 00:00:00 EST 5"),"taskName":"E_1","status":"E"},
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 5"),"endDate":new Date("Sun Nov 15 00:00:00 EST 10"),"taskName":"E_1","status":"E"},
+    //{"startDate":new Date("Sun Dec 09 01:21:00 EST 2012"),"endDate":new Date("Sun Dec 25 02:51:42 EST 2012"),"taskName":"E_1","status":"E"},
+    //{"startDate":new Date("Sun Dec 09 07:39:21 EST 2012"),"endDate":new Date("Sun Dec 25 07:43:22 EST 2012"),"taskName":"E_1","status":"E"},
+    //{"startDate":new Date("Sun Dec 09 08:46:59 EST 2012"),"endDate":new Date("Sun Dec 25 09:54:19 EST 2012"),"taskName":"E_1","status":"E"},
+    //{"startDate":new Date("Sun Dec 09 10:21:00 EST 2012"),"endDate":new Date("Sun Dec 25 10:51:42 EST 2012"),"taskName":"E_1","status":"E"},
+
+
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 0"),"endDate":new Date("Sun Nov 15 00:00:00 EST 5"),"taskName":"LG_1","status":"LG"},
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 5"),"endDate":new Date("Sun Nov 15 00:00:00 EST 10"),"taskName":"LG_1","status":"LG"},
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 10"),"endDate":new Date("Sun Nov 20 00:00:00 EST 15"),"taskName":"LG_1","status":"LG"},
+    //{"startDate":new Date("Sun Dec 09 08:27:35 EST 2012"),"endDate":new Date("Sun Dec 25 08:58:43 EST 2012"),"taskName":"LG_1","status":"LG"},
+    //{"startDate":new Date("Sun Dec 09 12:27:15 EST 2012"),"endDate":new Date("Sun Dec 25 12:54:56 EST 2012"),"taskName":"LG_1","status":"LG"},
+
+
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 0"),"endDate":new Date("Sun Nov 25 00:00:00 EST 15"),"taskName":"FP_1","status":"FP"},
+    //{"startDate":new Date("Sun Dec 09 01:40:11 EST 2012"),"endDate":new Date("Sun Dec 09 03:26:35 EST 2012"),"taskName":"FP_1","status":"FP"},
+    //{"startDate":new Date("Sun Dec 09 08:40:11 EST 2012"),"endDate":new Date("Sun Dec 09 08:46:35 EST 2012"),"taskName":"FP_1","status":"FP"},
+    //{"startDate":new Date("Sat Dec 08 23:12:24 EST 2012"),"endDate":new Date("Sun Dec 09 00:26:13 EST 2012"),"taskName":"FP_1","status":"FP"},
+
+
+    {"startDate":new Date("Sun Dec 01 00:00:00 EST 0"),"endDate":new Date("Sun Nov 25 " +
+            " 00:00:00 EST 15"),"taskName":"DC_1","status":"DC"},
+    //{"startDate":new Date("Sun Dec 09 01:08:42 EST 2012"),"endDate":new Date("Sun Dec 25 01:33:42 EST 2012"),"taskName":"DC_1","status":"DC"},
+    //{"startDate":new Date("Sun Dec 09 09:02:45 EST 2012"),"endDate":new Date("Sun Dec 25 09:48:56 EST 2012"),"taskName":"DC_1","status":"DC"},
+    //{"startDate":new Date("Sun Dec 09 11:08:42 EST 2012"),"endDate":new Date("Sun Dec 25 11:33:42 EST 2012"),"taskName":"DC_1","status":"DC"}
+
+    ];
+
+var taskStatus = {
+    "LG" : "bar-LG",
+    "FP" : "bar-FP",
+    "E" : "bar-E",
+    "DC" : "bar-DC",
+    "Fwr" : "bar-Fwr"
+};
+
+var taskNames = [ "Fwr_1", "E_1", "LG_1", "FP_1", "DC_1" ];
+
+tasks.sort(function(a, b) {
+    return a.endDate - b.endDate;
+});
+var maxDate = tasks[tasks.length - 1].endDate;
+tasks.sort(function(a, b) {
+    return a.startDate - b.startDate;
+});
+var minDate = tasks[0].startDate;
+
+var format = "%Y";
+
+var gantt = d3.gantt().taskTypes(taskNames).taskStatus(taskStatus).tickFormat(format);
+gantt(tasks);
